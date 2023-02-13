28ad1c29d80611d324415a6c71c710a2e3ae3c3d
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-02-14 02:00:01 +0300

    arcade

--- src/game_01/index.ts
+++ src/game_01/index.ts
@@ -1,39 +1,24 @@
 import { Types } from "phaser"
 import GameScene from "./scenes/GameScene"
 
-/*/
-const gameConfig: Types.Core.GameConfig = {
-    width: "100%",
-    height: "100%",
-    type: Phaser.AUTO,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    render: {
-        antialias: false,
-        pixelArt: true,
-        roundPixels: true
-    },
-    scene: GameScene
-}
-/*/
 const gameConfig: Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    // parent: 'phaser-container',
-    // backgroundColor: '#282c34',
+    width: 640, height: 480,
     backgroundColor: '#336699',
+    /*/
     scale: {
         // mode: Phaser.Scale.ScaleModes.RESIZE,
         // width: window.innerWidth,
         // height: window.innerHeight,
 
-        width: 800, height: 600, //[*]
+        width: 640, height: 480, //[*]
     },
+    /*/
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 },
+            debug: true,
+            // gravity: { y: 600 },
         },
     },
     scene: [GameScene],
--- src/game_01/scenes/GameScene.ts
+++ src/game_01/scenes/GameScene.ts
@@ -3,29 +3,101 @@
 
 import Phaser from 'phaser'
 
-let map, tileset, layer, player, cursors
-
+// const heroW = 16, heroH = 28
 
 export default class GameScene extends Phaser.Scene {
+    private ball: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | undefined
+    private cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined
+    private player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | undefined
+    private power: number = 0
+    private timer: Phaser.Time.TimerEvent | undefined
+
     constructor() {
         super('GameScene')
     }
 
     preload() {
-        // this.load.tilemap("level", "level.json", null, Phaser.Tilemap.TILED_JSON);
-        // this.load.tileset("tiles", "tiles.png", 32, 32);
+        this.load.tilemapTiledJSON('tilemap', 'assets/01/tiles/level.json')
+        this.load.image('tileset', 'assets/01/tiles/tiles.png')
+
         this.load.image("hero", "assets/01/hero.png")
+
+        //
+        this.load.image("ball", "assets/01/ball.png")
+        this.load.image("block", "assets/01/block.png")
+        //
     }
 
     create() {
-        const hero = this.add.image(400, 150, "hero")
-        this.tweens.add({
-            targets: hero,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
+        this.power = 0
+
+        const map = this.make.tilemap({ key: 'tilemap' })
+        const tileset = map.addTilesetImage('tileset', 'tileset')
+        const layer = map.createLayer('Tile Layer 1', tileset)
+        map.setCollision([ 1 ])
+
+        const player = this.player = this.physics.add.sprite(32, 416, "hero")
+        player.setOrigin(0,1)
+        this.physics.add.collider(player, layer)
+        player.setGravityY(400)
+
+        //
+        const ball = this.ball = this.physics.add.sprite(+this.sys.game.config.width/2,0,"ball")
+        ball.setGravityY(100)
+
+        // this.input.on('pointerdown', this.jump, this)
+        this.input.on('pointerdown', this.startJump, this)
+        this.input.on('pointerup', this.endJump, this)
+
+        let groundX = +this.sys.game.config.width / 2
+        let groundY = +this.sys.game.config.height * .95
+        let ground = this.physics.add.sprite(groundX, groundY,"block")
+        ground.displayWidth = +this.sys.game.config.width
+        ground.alpha = .5
+        this.physics.add.collider(ball, ground)
+        ground.setImmovable()
+        //
+
+
+        this.cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        this.player?.setVelocityX(0)
+        if (this.cursors?.left.isDown){
+            this.player?.setVelocityX(-150)
+        }
+        if (this.cursors?.right.isDown){
+            this.player?.setVelocityX(150)
+        }
+        if (this.cursors?.up.isDown && this.player?.body.blocked.down){
+            this.player?.setVelocityY(-250)
+        }
+    }
+
+    jump() {
+        this.ball?.setVelocityY(-100);
+    }
+
+    startJump() {
+        this.timer = this.time.addEvent({
+            delay: 100,
+            callback: this.tick,
+            callbackScope: this,
+            loop: true
         })
     }
+
+    endJump() {
+        this.timer?.remove()
+        this.ball?.setVelocityY(-this.power * 100)
+        this.power = 0
+    }
+
+    tick() {
+        if (this.power < 5) {
+            this.power += .1
+        }
+    }    
+
 }
