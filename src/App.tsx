25c21b0aac1321cc5f7750c3c9c146f635a5a206
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-02-14 15:54:27 +0300

    game 02

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useRef } from 'react'
 import './App.css'
 
 import { useGame } from "./hooks"
-import gameConfig from "./game_01"//_01
-import { handleClick } from "./game_01/utils"
+import gameConfig from "./game_02"
+import { handleClick } from "./game_02/utils"
 // import useWindowSize from "./useWindowSize"
 
 function App() {
