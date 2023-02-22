258dbe9a2ba9107815eb4ada917cf966d658a8f6
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-02-22 17:21:21 +0300

    group

--- src/game_04/scenes/GameScene.ts
+++ src/game_04/scenes/GameScene.ts
@@ -3,21 +3,56 @@
 
 import Phaser from 'phaser'
 
+const KEY_CRATE = 'crate'
+const INFO_FORMAT = `Size:       %1
+Spawned:    %2
+Despawned:  %3`
+
+type TCrate = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+
 export default class GameScene extends Phaser.Scene {
+    // private crateGroup?: Phaser.GameObjects.Group
+    private crateGroup?: Phaser.Physics.Arcade.Group
+    private infoText?: Phaser.GameObjects.Text
 
     constructor() {
         super('GameScene')
     }
 
     preload() {
-        this.load.image("crate", "assets/04/crate.png")
+        this.load.image(KEY_CRATE, "assets/04/crate.png")
     }
 
     create() {
+        this.crateGroup = this.physics.add.group({
+            defaultKey: KEY_CRATE,
+            maxSize: 5,
+            // visible: false,
+            // active: false
+        })
+
+        this.physics.add.collider(this.crateGroup, this.crateGroup)
+
+        this.infoText = this.add.text(16, 16, 'crate')
+
         this.input.on('pointerup', this.handlePointerUp, this)
     }
 
     update() {
+        if (!this.crateGroup || !this.infoText) return
+
+        const size = this.crateGroup.getLength()
+        const used = this.crateGroup.getTotalUsed()
+        const text = Phaser.Utils.String.Format(
+            INFO_FORMAT,
+            [
+                size,
+                used,
+                size - used
+            ]
+        )
+
+        this.infoText.setText(text)
     }
 
     handlePointerUp(pointer: Phaser.Input.Pointer) {
@@ -27,13 +62,45 @@ export default class GameScene extends Phaser.Scene {
         const a = this.input.hitTestPointer(pointer)
         console.log('[', a.length, ']', a)
         if (a.length) {
-            //
+            this.despawnCrate(a[0] as TCrate)
         }
         else {
-            const box = this.physics.add.sprite(pointer.x, pointer.y,"crate")
-            box.setInteractive({ useHandCursor: true })
-            box.body.setCollideWorldBounds(true)
+            // const box = this.physics.add.sprite(pointer.x, pointer.y,KEY_CRATE)
+            // box.setInteractive({ useHandCursor: true })
+            // box.body.setCollideWorldBounds(true)
+            this.spawnCrate(pointer.x, pointer.y)
         }
+    }
+
+    private spawnCrate(x: number, y: number) {
+        if (!this.crateGroup) return null
+
+        const crate: TCrate = this.crateGroup.get(x, y, KEY_CRATE)
+        if (!crate) return null
+
+        crate.alpha = 1
+        crate.scale = 1
+        crate.setVisible(true)
+        crate.setActive(true)
+        crate.setInteractive({ useHandCursor: true })
+        crate.body.setCollideWorldBounds(true)
+        crate.body.enable = true
+        return crate
+    }
+
+    private despawnCrate(crate: TCrate) {
+        // this.crateGroup!.killAndHide(crate)
+        this.tweens.add({
+            targets: crate,
+            // scale: 2,
+            alpha: 0,
+            duration: Phaser.Math.Between(500, 1500),
+            onComplete: (tween) => {
+                this.crateGroup!.killAndHide(crate)
+                this.tweens.killTweensOf(crate)
+                crate.body.enable = false
+            }
+        })
     }
 
 }
