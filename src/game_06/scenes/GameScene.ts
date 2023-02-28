7928ed800d902b9f86a6b0f2f7ba5c1e7a68234e
Oleg <35769958+trudoholic@users.noreply.github.com>
2023-03-01 00:31:28 +0300

    tabIndex

--- src/game_06/scenes/GameScene.ts
+++ src/game_06/scenes/GameScene.ts
@@ -55,6 +55,7 @@ export default class GameScene extends Phaser.Scene {
 
         const tileX = toCol(rnd) * tileSize, tileY = toRow(rnd) * tileSize
         const container = this.add.container(tileX, tileY, [ tile, text ])
+        container.tabIndex = rnd
         this.tiles.push(container)
 
         tile.alpha = 0
@@ -85,41 +86,29 @@ export default class GameScene extends Phaser.Scene {
             let moved = false
             // this.tiles.sort("x", Phaser.Group.SORT_ASCENDING)
             this.tiles.sort((a, b) => a.x - b.x)
-            // looping through each element in the group
             this.tiles.forEach(function(it){
-                // getting row and column starting from a one-dimensional array
-                var row = toRow(it.pos);
-                var col = toCol(it.pos);
-                // checking if we aren't already on the leftmost column (the tile can't move)
-                if(col>0){
-                    // setting a "remove" flag to false. Sometimes you have to remove tiles, when two merge into one
-                    var remove = false;
-                    // looping from column position back to the leftmost column
-                    for(i=col-1;i>=0;i--){
-                        // if we find a tile which is not empty, our search is about to end...
-                        if(fieldArray[row*4+i]!=0){
-                            // ...we just have to see if the tile we are landing on has the same value of the tile we are moving
-                            if(fieldArray[row*4+i]==fieldArray[row*4+col]){
-                                // in this case the current tile will be removed
-                                remove = true;
-                                i--;
+                const row = toRow(it.tabIndex)
+                const col = toCol(it.tabIndex)
+                if (col > 0) {
+                    let remove = false
+                    let i = 0
+                    for (i = col - 1; i >= 0; i--) {
+                        if (fieldArray[row * 4 + i] !== 0) {
+                            if (fieldArray[row * 4 + i] === fieldArray[row * 4 + col]) {
+                                remove = true
+                                i--
                             }
-                            break;
+                            break
                         }
                     }
-                    // if we can actually move...
-                    if(col!=i+1){
-                        // set moved to true
-                        moved=true;
-                        // moving the tile "item" from row*4+col to row*4+i+1 and (if allowed) remove it
-                        moveTile(item,row*4+col,row*4+i+1,remove);
+                    if (col !== i + 1) {
+                        moved = true
+                        // moveTile(item, row * 4 + col, row * 4 + i + 1, remove)
                     }
                 }
-            });
-            // completing the move
-            endMove(moved);
+            })
+            // endMove(moved)
         }
-
         /*/
     }
 
